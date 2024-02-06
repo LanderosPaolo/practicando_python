3649f7876f6ad5e37dd398ab0e781c2e6a4e452d
Paolo Landeros <landerospaolo@gmail.com>
2024-02-06 14:49:04 -0300

    first game

---
+++ Practicando/02.Juego-MadLib/madlib_game.py
@@ -0,0 +1,41 @@
+parte_del_cuerpo_plural = input('Ingresa una parte del cuerpo en plural: ')
+adjetivo_masculino_plural = input('Ingresa un adjetivo masculino en plural: ')
+adjetivo_masculino = input('Ingresa un adjetivo masculino: ')
+adjetivo_masculino_2 = input('Ingresa un segundo adjetivo masculino: ')
+lugar = input('Ingresa un lugar: ')
+animal = input('Ingresa un animal: ')
+parte_del_cuerpo = input('ingresa una parte del cuerpo: ')
+adjetivo = input('Ingresa un adjetivo: ')
+adjetivo_femenino_plural = input('Ingresa un adjetivo femenino en plural: ')
+adverbio = input('Ingresa un adverbio: ')
+interjeccion = input('Ingresa una interjección: ')
+adjetivo_femenino_plural_2 = input('Inregsa un segundo adjetivo femenino en plural: ')
+nombre_propio = input('Ingresa un nombre propio: ')
+nombre_femenino = input('Ingresa un nombre femenino: ')
+
+print(f'''
+Antiguamente, el ser humano caminada a cuatro "{parte_del_cuerpo_plural}",
+se expresaba mediante gruñidos "{adjetivo_masculino_plural}" y no sabía encender un "{adjetivo_masculino}" fuego.
+Esta es la historia del día en que la humanidad cambió para siempre (traducida del "{adjetivo_masculino_2}" idioma de las cavernas):
+-.  Hombre de las cavernas 1: 
+    En esta cueva hace más frío que en (el/ella) "{lugar}".
+    incluso con la piel del/de la "{animal}" más caliente no consigo que mi "{parte_del_cuerpo}" deje de tiritar.
+
+-.  Hombre de las cavernas 2:
+    Si al menos pudiésemos encontrar una manera de que el frío fuera más "{adjetivo}".
+
+-.  Hombre de las cavernas 1:"
+    Estoy aburrido. Creo que voy a jugar un rato con estas ramas "{adjetivo_femenino_plural}".
+
+-.  Hombre de las cavernas 2:
+    ¿Por qué no las frotas "{adverbio}" a ver qué pasa?
+
+-.  Hombre de las cavernas 1:
+    ¡"{interjeccion}"! Está saliendo humo de las "{adjetivo_femenino_plural_2}" ramas!
+
+-.  Hombre de las cavernas 2:
+    ¡Ay! ¡Está caliente! ¡Por el amor de "{nombre_propio}", hemos conseguido calor!
+
+-.  Hombre de las cavernas 1:
+    A partir de ahora llamaremos "fuego" a esta "{nombre_femenino}" mágica.
+''')
