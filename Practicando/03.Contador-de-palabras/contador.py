da68daff33b90ab658a8aae9649ecbd8c48526c9
Paolo Landeros <landerospaolo@gmail.com>
2024-02-08 19:12:46 -0300

    comentarios

--- Practicando/03.Contador-de-palabras/contador.py
+++ Practicando/03.Contador-de-palabras/contador.py
@@ -1,11 +1,10 @@
-# Creamos las funciones para los distintos casos
-
 # Funcion para contar cantidad de palabras en el texto
 def contador_palabras(texto):
     # Separamos el texto en palabras
     texto = texto.split()
-    # imprimimos la cantidad de de palabras en el texto
+    # Calculamos la cantidad de palabras
     cantidad_palabras = len(texto)
+    # Imprimimos el resultado
     if cantidad_palabras == 1:
         print(f'\nEl texto contiene "{cantidad_palabras}" palabra')
     else:
@@ -13,15 +12,20 @@ def contador_palabras(texto):
 
 # Funcion para contar cantidad de letras en el texto
 def contador_letras(texto):
+    # Calculamos la cantidad de letras
     cantidad_letras = sum(c.isalpha() for c in texto)
+    # Imprimimos el resultado
     if cantidad_letras == 1:
         print(f'\nEl texto contiene "{cantidad_letras}" letra')
     else:
         print(f'\nEl texto contiene "{cantidad_letras}" letras')
 
+# Bucle principal para permitir al usuario seleccionar y ejecutar operaciones
 while True:
     try:
+        # Bucle para manejar la entrada del usuario
         while True:
+            # Solicitamos al usuario que elija una opción
             try:
                 eleccion = int(input('''
 Que deseas hacer?
@@ -29,11 +33,13 @@ Que deseas hacer?
 [2]: Contar cantidad de letras.
 [3]: Salir.
 -->: '''))
+                # Salimos del bucle interno si la conversión a número es exitosa
                 break
                 
             except ValueError:
                 print('\n--> Has ingresado algo que no es un número, por favor, intenta de nuevo. <--')
 
+        # Ejecutamos la opción seleccionada por el usuario
         if eleccion == 1:
             texto = input('\nQue texto quieres analizar?: ')
             contador_palabras(texto)
@@ -52,10 +58,11 @@ Que deseas hacer?
 
     except Exception as e:
         print('Ha ocurrido un error:', e)
-        
+    
+    # Preguntamos al usuario si quiere realizar otra acción
     otra_accion = input('\n¿Quieres realizar otra accion? Y/N: ').lower()
 
-    # Si el usuario no quiere jugar otra historia, imprimimos un mensaje y salimos del bucle
+    # Salimos del bucle principal si el usuario no quiere realizar otra acción
     if otra_accion != 'y':
         print('\n¡Vuelve pronto!')
         break
