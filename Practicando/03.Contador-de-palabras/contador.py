25825570e4ebb1d0b5bd11619f2b49f6cf2c0fb2
Paolo Landeros <landerospaolo@gmail.com>
2024-02-08 19:18:49 -0300

    arreglo de escritura

--- Practicando/03.Contador-de-palabras/contador.py
+++ Practicando/03.Contador-de-palabras/contador.py
@@ -1,4 +1,4 @@
-# Funcion para contar cantidad de palabras en el texto
+# Función para contar cantidad de palabras en el texto
 def contador_palabras(texto):
     # Separamos el texto en palabras
     texto = texto.split()
@@ -10,7 +10,7 @@ def contador_palabras(texto):
     else:
         print(f'\nEl texto contiene "{cantidad_palabras}" palabras')
 
-# Funcion para contar cantidad de letras en el texto
+# Función para contar cantidad de letras en el texto
 def contador_letras(texto):
     # Calculamos la cantidad de letras
     cantidad_letras = sum(c.isalpha() for c in texto)
@@ -28,7 +28,7 @@ while True:
             # Solicitamos al usuario que elija una opción
             try:
                 eleccion = int(input('''
-Que deseas hacer?
+¿Que deseas hacer?
 [1]: Contar cantidad de palabras.
 [2]: Contar cantidad de letras.
 [3]: Salir.
@@ -41,11 +41,11 @@ Que deseas hacer?
 
         # Ejecutamos la opción seleccionada por el usuario
         if eleccion == 1:
-            texto = input('\nQue texto quieres analizar?: ')
+            texto = input('\n¿Qué texto quieres analizar?: ')
             contador_palabras(texto)
 
         elif eleccion == 2:
-            texto = input('\nQue texto quieres analizar?: ')
+            texto = input('\n¿Qué texto quieres analizar?: ')
             contador_letras(texto)
 
         elif eleccion == 3:
@@ -53,14 +53,14 @@ Que deseas hacer?
             break
 
         else:
-            print('''\n---> Has ingresado un numero no valido, intenta de nuevo. <-- ''')
+            print('''\n---> Has ingresado un número no válido, intenta de nuevo. <-- ''')
             continue
 
     except Exception as e:
         print('Ha ocurrido un error:', e)
     
     # Preguntamos al usuario si quiere realizar otra acción
-    otra_accion = input('\n¿Quieres realizar otra accion? Y/N: ').lower()
+    otra_accion = input('\n¿Quieres realizar otra acción? Y/N: ').lower()
 
     # Salimos del bucle principal si el usuario no quiere realizar otra acción
     if otra_accion != 'y':
