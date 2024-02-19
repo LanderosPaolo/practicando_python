b9b6174284a2bf0eb401993703857fbf5dc23803
Paolo Landeros <landerospaolo@gmail.com>
2024-02-19 17:47:33 -0300

    comentarios

--- Practicando/10.Extractor/extractor.py
+++ Practicando/10.Extractor/extractor.py
@@ -1,11 +1,16 @@
+# Se solicita el correo al usuario
 email = input('Ingresa tu correo electronico: ').strip('')
 
+# Se divide la cadena en dos separando desde el @
 usuario, dominio = email.split('@')
 
+# se separa el dominio en dos partes separando desde el .
 dominio = dominio.split('.')[0]
 
+# Se imprime el usuario
 print('usuario :'+ usuario)
 
+# Segun el dominio utilizado se da un mensaje "personalizado"
 if dominio == 'gmail':
     print('Saludos, estoy viendo que tu email está registrado con Google. ¡Eso es genial!')
 
